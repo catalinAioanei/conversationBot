3aa3beba04b9ff6850e5caf9fac377052c21658e
catalinAioanei <zcabcai@ucl.ac.uk>
2018-03-19 17:02:38 +0000

    clean convo

--- skills/newrequest_conversation.js
+++ skills/newrequest_conversation.js
@@ -114,7 +114,7 @@ module.exports = function(controller) {
 
                       }
                     } else {
-                      convo.say("Ahh, you got bamboozled again. Couldn't process your request");
+                      convo.say("We are sorry but we couldn't process your request. Something could be wrong with our servers. Try submitting your request at a later time.");
                     }
                   }
                 );
@@ -123,69 +123,6 @@ module.exports = function(controller) {
                 convo.say('I am sorry, but I am afraid I can not process that.');
                 convo.next();
             }
-
-            });
-        });
-
-    });
-
-
-    controller.hears(['^question$'], 'direct_message,direct_mention,bot_message', function(bot, message) {
-
-        bot.createConversation(message, function(err, convo) {
-
-            // create a path for when a user says YES
-            convo.addMessage({
-                    text: 'How wonderful.',
-            },'yes_thread');
-
-            // create a path for when a user says NO
-            // mark the conversation as unsuccessful at the end
-            convo.addMessage({
-                text: 'Cheese! It is not for everyone.',
-                action: 'stop', // this marks the converation as unsuccessful
-            },'no_thread');
-
-            // create a path where neither option was matched
-            // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
-            convo.addMessage({
-                text: 'Sorry I did not understand. Say `yes` or `no`',
-                action: 'default',
-            },'bad_response');
-
-            // Create a yes/no question in the default thread...
-            convo.ask('Do you like cheese?', [
-                {
-                    pattern:  bot.utterances.yes,
-                    callback: function(response, convo) {
-                        convo.gotoThread('yes_thread');
-                    },
-                },
-                {
-                    pattern:  bot.utterances.no,
-                    callback: function(response, convo) {
-                        convo.gotoThread('no_thread');
-                    },
-                },
-                {
-                    default: true,
-                    callback: function(response, convo) {
-                        convo.gotoThread('bad_response');
-                    },
-                }
-            ]);
-
-            convo.activate();
-
-            // capture the results of the conversation and see what happened...
-            convo.on('end', function(convo) {
-
-                if (convo.successful()) {
-                    // this still works to send individual replies...
-                    bot.reply(message, 'Let us eat some!');
-
-                    // and now deliver cheese via tcp/ip...
-                }
 
             });
         });
